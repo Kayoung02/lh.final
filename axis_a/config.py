3c203ba01bd5e318036603406b88522a335873b9
Kayoung02 <skymomy0414@gmail.com>
2026-08-25 23:54:47 +0900

    Update config.py

--- axis_a/config.py
+++ axis_a/config.py
@@ -1,1 +1,21 @@
+from pathlib import Path
 
+
+DATA_PATH = Path(
+    "data/서울시_공동주택_1차전처리.csv"
+)
+
+BOUNDARY_PATH = Path(
+    "data/seoul_gu.geojson"
+)
+
+BOUNDARY_URL = (
+    "https://raw.githubusercontent.com/"
+    "southkorea/seoul-maps/master/kostat/2013/json/"
+    "seoul_municipalities_geo_simple.json"
+)
+
+EXCLUDED_COMPANIES = [
+    "기타",
+    "미상",
+]
