ef98c28cd370eee33435a4d2cd31c0b34b96d005
Kayoung02 <skymomy0414@gmail.com>
2026-08-25 22:42:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import os
 import numpy as np
 import pandas as pd
 import streamlit as st
+import folium
 
+st.set_page_config(layout="wide", page_title="시공사 점유율 & 정비사업 대시보드")
 st.title("LH 데이터 분석 앱")
 data_path = "./data"
 서울시APT = pd.read_csv(f"{data_path}/서울시_공동주택_1차전처리.csv")
-
-st.subheader("서울시 APT 데이터 상위 5개")
-st.dataframe(서울시APT.head())
