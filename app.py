5f23e2a86607f56d9555af74838f97cc6d37be08
Kayoung02 <skymomy0414@gmail.com>
2026-08-25 22:28:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.title("LH 데이터 분석 앱")
 data_path = "./data"
-서울시APT = pd.read_csv(f"{data_path}/서울시_공동주택_1차전처리.csv", encoding="cp949")
+서울시APT = pd.read_csv(f"{data_path}/서울시_공동주택_1차전처리.csv")
 
 st.subheader("서울시 APT 데이터 상위 5개")
 st.dataframe(서울시APT.head())
